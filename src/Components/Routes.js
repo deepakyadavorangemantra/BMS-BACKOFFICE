7ae15a976dd436f50e40450c2b81df94a3a03e78
deepak <93deep93@gmail.com>
2020-12-14 18:51:34 +0530

    add s3 image upload function.

--- src/Components/Routes.js
+++ src/Components/Routes.js
@@ -2,15 +2,16 @@ import React from "react"
 import { Route } from "react-router-dom"
 import Login from './Login'
 import Sidenav from './Sidenav';
-
+import AwsImageUpload from '../Pages/AwsImageUpload';
 
 
 
 
 const Routes = () => {
     return(
-        <div>
+        <div> 
         <Route exact path='/' component={Login}></Route>
+        <Route exact path='/awsimageupload' component={AwsImageUpload}></Route>
         <Route exact path='/dashboard' component={Sidenav}></Route>
         {window.location.pathname != '/' && window.location.pathname != '/dashboard' ? <Sidenav></Sidenav> : <Route/> }
     
---
+++ src/Pages/AwsImageUpload.js
@@ -0,0 +1,49 @@
+import React, { useRef } from "react";
+import S3 from "react-aws-s3";
+
+function AwsImageUpload() {
+  const fileInput = useRef();
+  const handleClick = (event) => {
+    event.preventDefault();
+    let file = fileInput.current.files[0];
+    let newFileName = fileInput.current.files[0].name.replace(/\..+$/, "");
+    const config = {
+      // bucketName: process.env.REACT_APP_BUCKET_NAME,
+      // dirName: process.env.REACT_APP_DIR_NAME /* optional */,
+      // region: process.env.REACT_APP_REGION,
+      // accessKeyId: process.env.REACT_APP_ACCESS_ID,
+      // secretAccessKey: process.env.REACT_APP_ACCESS_KEY,
+
+      bucketName: 'bmsbucketforimages',
+      dirName: '/images/', /* optional */
+      region: 'ap-south-1',
+      accessKeyId: 'AKIAUXRQWUVJUYIDJI6T',
+      secretAccessKey: '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',
+      s3Url: 'https://bmsbucketforimages.s3.ap-south-1.amazonaws.com', /* optional */
+    };
+    const ReactS3Client = new S3(config);
+    debugger;
+    ReactS3Client.uploadFile(file, newFileName).then((data) => {
+      console.log(data);
+      if (data.status === 204) {
+        console.log("success");
+      } else {
+        console.log("fail");
+      }
+    });
+  };
+  return (
+    <div style={{ marginTop:'50%', marginLeft:'50%'}}>
+      <form className='upload-steps' onSubmit={handleClick}>
+        <label>
+          Upload file:
+          <input style={{ display: 'block' }} type='file' ref={fileInput} />
+        </label>
+        <br />
+        <button type='submit'>Upload</button>
+      </form>
+    </div>
+  );
+}
+
+export default AwsImageUpload;
